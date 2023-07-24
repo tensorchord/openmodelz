df9d7f02ec53d55915355531ab24841a822ae330
Ce Gao <cegao@tensorchord.ai>
2023-07-24 16:02:34 +0800

    chore: Remove charts and add public ip support (#48)
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- mdz/hack/helm-generate.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env bash
-
-set -e
-
-cd "$(dirname "$0")/.."
-
-# Create the single YAML file from helm chart.
-helm template ../charts/openmodelz --namespace openmodelz --set agent.ingress.enabled=true --set agent.ingress.ipToDomain=true > pkg/server/openmodelz.yaml
-cd - > /dev/null
