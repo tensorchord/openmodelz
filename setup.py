2a0d9f77dc6b0b595301be6ef934d1ae3d336af0
Keming <kemingyang@tensorchord.ai>
2023-08-01 19:50:27 +0800

    chore: add meta data to setup (#91)
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from setuptools.command.build_ext import build_ext
 from setuptools_scm import get_version
 
 
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
 class bdist_wheel_universal(bdist_wheel):
     def get_tag(self):
         *_, plat = super().get_tag()
@@ -40,6 +43,13 @@ class ModelzBuildExt(build_ext):
 setup(
     name="openmodelz",
     use_scm_version=True,
+    description="Simplify machine learning deployment for any environments.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tensorchord/openmodelz",
+    license="Apache License 2.0",
+    author="TensorChord",
+    author_email="modelz@tensorchord.ai",
     packages=find_packages("mdz"),
     include_package_data=True,
     data_files=[("bin", ["mdz/bin/mdz"])],
